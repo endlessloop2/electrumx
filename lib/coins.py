5666d21088ca88ee5bb99db7c741bc2b61f5d839
EndlessLoop <30296511+endlessloop2@users.noreply.github.com>
2017-10-08 22:51:25 -0300

    Update coins.py

--- lib/coins.py
+++ lib/coins.py
@@ -1086,3 +1086,15 @@ class Teslacoin(Coin):
     DAEMON = daemon.LegacyRPCDaemon
     DESERIALIZER = DeserializerTxTime
     HEADER_HASH = None
+    @classmethod
+    def header_hash(cls, header):
+        '''Given a header return the hash.'''
+        if cls.HEADER_HASH is None:
+            import scrypt
+            cls.HEADER_HASH = lambda x: scrypt.hash(x, x, 1024, 1, 1, 32)
+
+        version, = struct.unpack('<I', header[:4])
+        if version > 6:
+            return super().header_hash(header)
+        else:
+            return cls.HEADER_HASH(header)
